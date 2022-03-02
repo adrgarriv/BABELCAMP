959523b564c00ae5ff003d78ab9bef303786b832
Adri Garcia <adrigarciariv@gmail.com>
2022-03-02 13:06:38 +0100

    Commit de suma

---
+++ Ejercicio 1 Git/Suma.java
@@ -0,0 +1,18 @@
+public class Suma 
+{
+	private int total=0;
+	
+	public Suma() {}
+	
+	public void suma(int a, int b)
+	{
+		System.out.println("El resultado de la suma es "+(a+b));
+	}
+	
+	public void suma_acumulada(int a)
+	{
+		total+=a;
+		System.out.println("El resultado de la suma es "+(total));
+	}
+	
+}
