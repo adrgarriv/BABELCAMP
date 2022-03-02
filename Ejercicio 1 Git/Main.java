3e496b7e2b3a503c37590b4c8d9501bb693cc227
Adri Garcia <adrigarciariv@gmail.com>
2022-03-02 13:17:12 +0100

    Commit pruebas suma

---
+++ Ejercicio 1 Git/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Suma suma = new Suma();
+		suma.suma(2,3);
+		suma.suma_acumulada(5);
+		suma.suma_acumulada(6);
+    }
+}
